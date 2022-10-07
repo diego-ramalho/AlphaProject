d08c7b00df97d162c529196dfcc26600f8207a49
diego-ramalho <diego.s.ramalho@gmail.com>
2022-10-07 18:47:09 +0100

    New Migration

---
+++ Microservices/Migrations/20221007174301_RegistersNewAttr.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiTemplate.Migrations
+{
+    public partial class RegistersNewAttr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adviser",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fee",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinalSalePrice",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Particular",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RealEstate",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reduction",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SaleDate",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adviser",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "Fee",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "FinalSalePrice",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "Particular",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "RealEstate",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "Reduction",
+                table: "Registers");
+
+            migrationBuilder.DropColumn(
+                name: "SaleDate",
+                table: "Registers");
+        }
+    }
+}
